6c075750da3a2aa6df99c4876c1e98211aeebc2e
Dallas <118672953+Dallas-rdx@users.noreply.github.com>
2025-07-11 22:47:38 +0530

    initial commit of react project

---
+++ src/utils/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Owais Toheed",
+  profile: '../../public/profile.jpg',
+  designation: "Student",
+  description: "Hi, I'm Owais Toheed, a passionate and dedicated CSE student. I'm driven by a deep interest in Computer Science and love bringing ideas to life through code and design. I'm a fast learner with a strong self-learning attitude and always eager to explore new technologies and innovative projects. I enjoy blending logic with creativity to solve problems and build intuitive user experiences. I’m always open to opportunities that allow me to grow, collaborate, and contribute meaningfully in the world of tech.",
+  email: 'owaistoheed34501731@gmail.com',
+  phone: '+91 6006393403',
+  address: 'kharar punjab, India',
+  github: 'https://github.com/Dallas-rdx',
+  
+  instagram: 'https://www.instagram.com/p/DKSX025zIBreu-3L_EnSls0dmOlqHcoOh3DU_E0/?igsh=cDh1MWczcDh1cW13',
+  linkedIn: 'https://www.linkedin.com/in/owais-toheed-85b5b921b/',
+  twitter: 'https://x.com/Owaistoheed1?t=vkbIMg-H0FXdeb0cpn1YxQ&s=09',
+
+  
+  devUsername: "Owais Toheed",
+  resume: "https://drive.google.com/file/d/183PYxRtJlOhWQI1m-rqJWHyw9Y8j9Gl5/view?usp=drivesdk"
+}
---
+++ src/utils/projects.js
@@ -0,0 +1,56 @@
+export const projects = [
+    {
+        image: "/image/minecraft_tetris.png",
+        title: "Minecraft Tetris",
+        subtitle: "Using HTML, CSS, and JavaScript",
+        handle: "by owais toheed",
+        borderColor: "#3B82F6",
+        gradient: "linear-gradient(145deg, #3B82F6, #000)",
+        
+    },
+    {
+        image: "/image/tictactoe.png",
+        title: "TicTacToe",
+        subtitle: "Using JAVA",
+        handle: "by owais toheed",
+        borderColor: "#10B981",
+        gradient: "linear-gradient(180deg, #10B981, #000)",
+       
+    },
+    {
+        image: "/image/snake_game.png",
+        title: "Snake Game",
+        subtitle: "Using HTML, CSS, and JavaScript",
+        handle: "by owais toheed",
+        borderColor: "#F59E0B",
+        gradient: "linear-gradient(210deg, #F59E0B, #000)",
+       
+    },
+    {
+        image: "/image/matrix_magic.png",
+        title: "Matrix Magic",
+        subtitle: "Using HTML, CSS, and JavaScript",
+        handle: "by owais toheed",
+        borderColor: "#EF4444",
+        gradient: "linear-gradient(195deg, #EF4444, #000)",
+        
+    },
+    {
+        image: "/image/3in1.png",
+        title: "3 in 1 Project",
+        subtitle: "Using HTML, CSS, and JavaScript",
+        handle: "by owais toheed",
+        borderColor: "#8B5CF6",
+        gradient: "linear-gradient(160deg, #8B5CF6, #000)",
+        
+    },
+    {
+        image: "/image/music_player.png",
+        title: "Music Player",
+        subtitle: "Using HTML, CSS, and JavaScript",
+        handle: "by owais toheed",
+        borderColor: "#EC4899",
+        gradient: "linear-gradient(175deg, #EC4899, #000)",
+       
+    },
+];
