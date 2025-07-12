b013c6205b2faa2689d8baf16441651a22641982
Dallas <118672953+Dallas-rdx@users.noreply.github.com>
2025-07-12 14:08:51 +0530

    Initial commit of React project files

---
+++ src/utils/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+    {
+        id: 1,
+        title: "B-Tech in Computer Science",
+        company: "Baba Ghulam Shah Badshah University, J&K",
+        duration: "(August 2021 - may 2025)",
+    },
+    {
+        id: 2,
+        title: "Senior Secondary",
+        company: "Govt. Higher Secondary School, Mendhar",
+        duration: "(february 2020 - February 2021)",
+    },
+    {
+        id: 3,
+        title: "Secondary",
+        company: "Muqaddas Academy, Mendhar",
+        duration: "(Jan 2018 - jan 2019)",
+    },
+];
---
+++ src/utils/skills.js
@@ -0,0 +1,13 @@
+export const skillsData = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React",
+    "MySQL",
+    "C",
+    "JAVA",
+    "PowerBI",
+    "Tailwind",
+    "Figma",
+    "MaterialUI",
+];
